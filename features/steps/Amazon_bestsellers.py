86b45979f0ea789a3bcd3cbf6a87758895c33cd3
nurhusien1970 <omaridris5315@gmai>
2021-06-27 11:53:39 -0500

    HW6 Question #2 bonus

--- features/steps/Amazon_bestsellers.py
+++ features/steps/Amazon_bestsellers.py
@@ -10,7 +10,7 @@ def best_seller_tab(context):
 @when('Find Best sellers tabs')
 def best_sellers_nav_box(context):
 
-    context.driver.find_elements(By.CSS_SELECTOR, 'div#zg_tabs ul a')
+    context.window = context.driver.find_elements(By.CSS_SELECTOR, 'div#zg_tabs ul a')
 
 
 @then('Verify {nav_link} boxes are present')
@@ -20,6 +20,12 @@ def verify_count_box(context, nav_link):
     assert nav_link == count_tabs, f'expected {nav_link}, but got {count_tabs}'
 
 
+@then('loop 5 links and verify they match the side menu text')
+def loop_five_links(context):
+    context.driver.get('https://www.amazon.com/gp/bestsellers/?ref_=nav_cs_bestsellers')
+    count = context.driver.find_elements(By.CSS_SELECTOR, 'div#zg_tabs ul a')
 
-
+    for i in count:
+        i.click()
+        assert i.text in count[i]
 
--- features/tests/Amazon_bestsellers.feature
+++ features/tests/Amazon_bestsellers.feature
@@ -5,5 +5,9 @@ Feature: # Count Amazon Best Sellers clickable link boxes
   Scenario: Amazon Best sellers navigation link boxes
     # Enter steps here
 Given best Amazon sellers page
-    When Find Best sellers tabs
-    Then Verify 5 boxes are present
+
+    Scenario: Amazon Best sellers navigation link boxes LOOP
+      Given best Amazon sellers page
+      When Find Best sellers tabs
+      Then Verify 5 boxes are present
+      Then loop 5 links and verify they match the side menu text
